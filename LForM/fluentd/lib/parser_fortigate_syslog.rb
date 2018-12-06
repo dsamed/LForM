6ee707f77011d6bcd0f664cdc5e6b12fff485afa
dsamed <same@mac.com>
2018-12-06 09:43:26 +0900

    Update parser_fortigate_syslog.rb

--- LForM/fluentd/lib/parser_fortigate_syslog.rb
+++ LForM/fluentd/lib/parser_fortigate_syslog.rb
@@ -1,5 +1,5 @@
 require 'fluent/plugin/parser'
- class Fluent::TextParser::Parser
+ class Fluent::Parser::TextParser
   class SyslogParser < Parser
    Plugin.register_parser('fortigate_syslog', self)
 
